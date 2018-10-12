ba7459dfe3aa99c84922d0fa2e11fcf3c370f031
Darshan Chheda <40024710+dc250999@users.noreply.github.com>
2018-10-12 18:53:03 +0530

    Add files via upload

---
+++ Books.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Books extends Book
+{
+	String name,author;
+	int ID;
+	Books(){}
+	Books(String name,String author,int ID)
+	{
+		this.name=name;
+		this.author=author;
+		this.ID=ID;
+	}
+}
